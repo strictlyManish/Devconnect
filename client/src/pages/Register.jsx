637ed50a767cf16127853d0c947805dfcdf44458
strictlyManish <rajzmanish94@gmail.com>
2025-11-15 21:25:34 +0530

    Register page and login page creation

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Register() {
   return (
-    <div>Register</div>
+    <div className='flex'>
+      <div>
+        <div>
+          <h1>Welcome Connections</h1>
+          <p className='font-serif'>Please Enter your Details</p>
+        </div>
+        <form>
+          <label htmlFor="username">username</label>
+          <input type="text" placeholder='username' id='username' />
+          <label htmlFor="username">username</label>
+          <input type="email" placeholder='en' />
+        </form>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
